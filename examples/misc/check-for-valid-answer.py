49a7612bedad069db9e0791f23051c65e7a7d4cc
gersteinj <gersteinj@gmail.com>
2019-01-03 10:08:07 -0500

    added check for valid answer example

---
+++ examples/misc/check-for-valid-answer.py
@@ -0,0 +1,4 @@
+answer = input('Choose the character')
+possible_answers = ['John Watson', 'Mrs. Hudson', 'Sherlock Holmes']
+while answer not in possible_answers:
+    answer = input('Choose the character')
