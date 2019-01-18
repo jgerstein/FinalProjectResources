fa6d11507cb24a1d5c83311738eaaa5e3a3fbea6
gersteinj <gersteinj@gmail.com>
2019-01-18 10:10:41 -0500

    updated grid example

--- examples/grids/full-grid.py
+++ examples/grids/full-grid.py
@@ -26,11 +26,22 @@ class Grid:
                 row.append(Cell(x, y, True))
             self.grid.append(row)
     
+    def make_word(self, word, direction, x, y):
+        for n in range(len(word)):
+            if direction == 'horizontal':
+                self.grid[y][x+n].value = word[n]
+            elif direction == 'vertical':
+                self.grid[y+n][x].value = word[n]
+
     def show_grid(self):
         for row in self.grid:
             for cell in row:
                 cell.show_cell()
             print("")
+        print("-"*30)
 
-grid = Grid(10, 5)
+grid = Grid(3, 5)
+grid.show_grid()
+grid.make_word('cat', 'horizontal', 0, 0)
+grid.make_word('cow', 'vertical', 0, 0)
 grid.show_grid()
